21fa1e6e2a89d95856c31335391aaf7ff78671f8
Tatiana Chinchilla <tatianachin@gmail.com>
2025-03-31 20:18:29 -0400

    Lesson 3 Props Common Component Props and State

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,3 +1,5 @@
+import TodoListItem from "./TodoListItem"
+
 function TodoList(){
     const todos = [
         {id: 1, title: "review resources"},
@@ -6,8 +8,12 @@ function TodoList(){
     ]
     return(
         <ul>
-        {todos.map(todo => <li key={todo.id}>{todo.title}</li>)}
+        {todos.map(todo => <TodoListItem key={todo.id} todo={todo} />)}
         </ul>
     )
 }
 export default TodoList
+
+/**
+ * <li key={todo.id}>{todo.title}</li>
+ */
