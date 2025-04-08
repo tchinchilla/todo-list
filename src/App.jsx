880802872af1f853af3a818bb2be003766780149
Tatiana Chinchilla <tatianachin@gmail.com>
2025-04-08 17:36:51 -0400

    Lesson 4 Basic Hooks Passing Props Events Updating State

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,16 @@ import TodoList from './TodoList'
 
 function App() {
   
-  const [newTodo, setnewTodo] = useState("Initial value")
+  const [todoList, setTodoList] = useState([])
+  function handleAddTodo(newTodo){
+    setTodoList([...todoList, { id: Date.now(), title: newTodo }])
+  }
 
   return (
 <div>
   <h1>My To-do</h1>
-    <TodoForm/>
-    <p>{ newTodo }</p>
-    <TodoList/>
+    <TodoForm onAddTodo={handleAddTodo}/>
+    <TodoList todoList={ todoList }/>
 </div>
   )
 }
